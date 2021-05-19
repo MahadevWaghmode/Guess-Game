c3c4429e6ca41cd0a73e6d0403624eb1ccb2410e
MahadevWaghmode <45910438+MahadevWaghmode@users.noreply.github.com>
2021-05-19 19:29:33 +0530

    update

--- js/script.js
+++ js/script.js
@@ -4,9 +4,6 @@ var flag = 0;
 var arr = [["img/1.png", "img/2.png", "img/3.png"], ["img/4.png", "img/5.png", "img/6.png"], ["img/7.png", "img/8.png", "img/9.png"], ["img/10.png", "img/11.png", "img/12.png"], ["img/13.png", "img/2D.png", "img/3D.png"], ["img/5H.png", "img/6S.png", "img/8D.png"], ["img/9D.png", "img/10H.png", "img/AS.png"]];
 
 function display() {
-    console.log(arr);
-    document.getElementById("dbtn").style.display="none";
-    document.getElementById("main").style.display="";
     var cards = document.getElementsByClassName("card");
     for (var i = 0; i < 7; i++) {
         for (var j = 0; j < 3; j++) {
@@ -15,10 +12,13 @@ function display() {
             count++;
         }
 }count=0;
+    $("#cards img").each(function(i){
+        $(this).delay(50*i).fadeIn(500);
+      })
 }
 function shuffle()
 {
-    if(flag<3){
+
         var sel= document.getElementById("sbox");
         switch(sel.options[sel.selectedIndex].value){
             case "1":
@@ -38,6 +38,9 @@ function shuffle()
                 for(var i=0;i<7;i++){
                     arr.push(list1.splice(0,3));
                 }
+                $("#cards img").each(function(i){
+                    $(this).delay(50*i).fadeOut(500);
+                  })
                 display();
                 break;
             case "2":
@@ -57,6 +60,9 @@ function shuffle()
                 for(var i=0;i<7;i++){
                     arr.push(list1.splice(0,3));
                 }
+                $("#cards img").each(function(i){
+                    $(this).delay(50*i).fadeOut(500);
+                  })
                display();
                 break;
             case "3":
@@ -76,16 +82,12 @@ function shuffle()
                 for(var i=0;i<7;i++){
                     arr.push(list1.splice(0,3));
                 }
+                $("#cards img").each(function(i){
+                    $(this).delay(50*i).fadeOut(500);
+                  })
                display();
                 break;
         }
 
-    }
-    else{
-        display()
-        console.log(arr)
-        document.getElementById("main").style.display="none";
-        document.getElementById("res").src=arr[3][1];
-        document.getElementById("result").style.display="";
-    }
+    
 }
